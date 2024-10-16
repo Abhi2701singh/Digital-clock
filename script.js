83aafa98b3101b19c57e54d59e82b1552113fd5f
ABHINAV SINGH <162627370+Abhi2701singh@users.noreply.github.com>
2024-10-16 20:15:18 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function clock() {
-    const d = new Date;
-    var ind = d.toLocaleTimeString('en-US', { timeZone: 'Asia/Kolkata', });
-    document.querySelector('#india h1').innerHTML = ind;
-}
-clock()
-setInterval(clock, 1000)
